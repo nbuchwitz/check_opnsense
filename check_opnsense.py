932e4487392171ecba891448011a223ad88817d3
Nicolai Buchwitz <nb+github@tipi-net.de>
2024-06-30 21:18:35 +0200

    style: Format code with black
    Signed-off-by: Nicolai Buchwitz <nb+github@tipi-net.de>

--- check_opnsense.py
+++ check_opnsense.py
@@ -47,8 +47,8 @@ class NagiosState(Enum):
 
 
 class CheckOPNsense:
-    VERSION = '0.1.0'
-    API_URL = 'https://{host}:{port}/api/{uri}'
+    VERSION = "0.1.0"
+    API_URL = "https://{host}:{port}/api/{uri}"
 
     options = {}
     perfdata = []
@@ -65,34 +65,31 @@ class CheckOPNsense:
     def output(self, returnCode, message):
         prefix = returnCode.name
 
-        message = '{} - {}'.format(prefix, message)
+        message = "{} - {}".format(prefix, message)
 
         print(message)
         sys.exit(returnCode.value)
 
     def getURL(self, part):
-        return self.API_URL.format(
-            host=self.options.hostname,
-            port=self.options.port,
-            uri=part)
+        return self.API_URL.format(host=self.options.hostname, port=self.options.port, uri=part)
 
-    def request(self, url, method='get', **kwargs):
+    def request(self, url, method="get", **kwargs):
         response = None
         try:
-            if method == 'post':
+            if method == "post":
                 response = requests.post(
                     url,
                     verify=not self.options.api_insecure,
                     auth=(self.options.api_key, self.options.api_secret),
-                    data=kwargs.get('data', None),
+                    data=kwargs.get("data", None),
                     timeout=CHECK_API_TIMEOUT,
                 )
-            elif method == 'get':
+            elif method == "get":
                 response = requests.get(
                     url,
                     auth=(self.options.api_key, self.options.api_secret),
                     verify=not self.options.api_insecure,
-                    params=kwargs.get('params', None),
+                    params=kwargs.get("params", None),
                     timeout=CHECK_API_TIMEOUT,
                 )
             else:
@@ -100,10 +97,14 @@ class CheckOPNsense:
         except requests.exceptions.ConnectTimeout:
             self.output(NagiosState.UNKNOWN, "Could not connect to OPNsense: Connection timeout")
         except requests.exceptions.SSLError:
-            self.output(NagiosState.UNKNOWN, "Could not connect to OPNsense: Certificate validation failed")
+            self.output(
+                NagiosState.UNKNOWN, "Could not connect to OPNsense: Certificate validation failed"
+            )
         except requests.exceptions.ConnectionError as e:
             print(e)
-            self.output(NagiosState.UNKNOWN, "Could not connect to OPNsense: Failed to resolve hostname")
+            self.output(
+                NagiosState.UNKNOWN, "Could not connect to OPNsense: Failed to resolve hostname"
+            )
 
         if response.ok:
             return response.json()
@@ -122,7 +123,7 @@ class CheckOPNsense:
     def check(self):
         self.checkResult = NagiosState.OK
 
-        if self.options.mode == 'updates':
+        if self.options.mode == "updates":
             self.checkUpdates()
         else:
             message = "Check mode '{}' not known".format(self.options.mode)
@@ -131,45 +132,75 @@ class CheckOPNsense:
         self.checkOutput()
 
     def parseOptions(self):
-        p = argparse.ArgumentParser(description='Check command OPNsense firewall monitoring')
+        p = argparse.ArgumentParser(description="Check command OPNsense firewall monitoring")
 
-        api_opts = p.add_argument_group('API Options')
+        api_opts = p.add_argument_group("API Options")
 
-        api_opts.add_argument("-H", "--hostname", required=True, help="OPNsense hostname or ip address")
-        api_opts.add_argument("-p", "--port", required=False, dest='port', help="OPNsense https-api port", default=443, type=int)
-        api_opts.add_argument("--api-key", dest='api_key', required=True,
-                              help="API key (See OPNsense user manager)")
-        api_opts.add_argument("--api-secret", dest='api_secret', required=True,
-                              help="API key (See OPNsense user manager)")
-        api_opts.add_argument("-k", "--insecure", dest='api_insecure', action='store_true', default=False,
-                              help="Don't verify HTTPS certificate")
+        api_opts.add_argument(
+            "-H", "--hostname", required=True, help="OPNsense hostname or ip address"
+        )
+        api_opts.add_argument(
+            "-p",
+            "--port",
+            required=False,
+            dest="port",
+            help="OPNsense https-api port",
+            default=443,
+            type=int,
+        )
+        api_opts.add_argument(
+            "--api-key", dest="api_key", required=True, help="API key (See OPNsense user manager)"
+        )
+        api_opts.add_argument(
+            "--api-secret",
+            dest="api_secret",
+            required=True,
+            help="API key (See OPNsense user manager)",
+        )
+        api_opts.add_argument(
+            "-k",
+            "--insecure",
+            dest="api_insecure",
+            action="store_true",
+            default=False,
+            help="Don't verify HTTPS certificate",
+        )
 
-        check_opts = p.add_argument_group('Check Options')
+        check_opts = p.add_argument_group("Check Options")
 
-        check_opts.add_argument("-m", "--mode",
-                                choices=('updates',),
-                                required=True,
-                                help="Mode to use.")
-        check_opts.add_argument('-w', '--warning', dest='treshold_warning', type=float,
-                                help='Warning treshold for check value')
-        check_opts.add_argument('-c', '--critical', dest='treshold_critical', type=float,
-                                help='Critical treshold for check value')
+        check_opts.add_argument(
+            "-m", "--mode", choices=("updates",), required=True, help="Mode to use."
+        )
+        check_opts.add_argument(
+            "-w",
+            "--warning",
+            dest="treshold_warning",
+            type=float,
+            help="Warning treshold for check value",
+        )
+        check_opts.add_argument(
+            "-c",
+            "--critical",
+            dest="treshold_critical",
+            type=float,
+            help="Critical treshold for check value",
+        )
 
         options = p.parse_args()
 
         self.options = options
 
     def checkUpdates(self):
-        url = self.getURL('core/firmware/status')
+        url = self.getURL("core/firmware/status")
         data = self.request(url)
 
-        if data['status'] == 'ok' and data['status_upgrade_action'] == 'all':
-            count = data['updates']
+        if data["status"] == "ok" and data["status_upgrade_action"] == "all":
+            count = data["updates"]
 
             self.checkResult = NagiosState.WARNING
             self.checkMessage = "{} pending updates".format(count)
 
-            if data['upgrade_needs_reboot']:
+            if data["upgrade_needs_reboot"]:
                 self.checkResult = NagiosState.CRITICAL
                 self.checkMessage = "{}. Subsequent reboot required.".format(self.checkMessage)
         else:
@@ -185,4 +216,3 @@ class CheckOPNsense:
 
 opnsense = CheckOPNsense()
 opnsense.check()
-
